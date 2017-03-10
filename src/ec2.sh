30405301e6ac2bf95a58beeb11d473bb689bbd70
Anthony Abercrombie <amarinusha@gmail.com>
2017-03-10 09:50:43 -0800

    Update ec2.sh

--- src/ec2.sh
+++ src/ec2.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# Launch our instance, which ec2_bootstrap.sh will initialize
+# Launches a r3.xlarge ECS instance, which ec2_bootstrap.sh will initialize and setup.
+# User needs to provide a key (key-name) and set configure aws with $aws config
 aws ec2 run-instances \
     --image-id ami-4ae1fb5d \
-    --key-name agile_data_science \
+    --key-name keypair \
     --user-data file://aws/ec2_bootstrap.sh \
     --instance-type r3.xlarge \
     --ebs-optimized \
-    --placement "AvailabilityZone=us-east-1d" \
+    --placement "AvailabilityZone=us-west-2a" \
     --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"DeleteOnTermination":false,"VolumeSize":1024}}' \
     --count 1
